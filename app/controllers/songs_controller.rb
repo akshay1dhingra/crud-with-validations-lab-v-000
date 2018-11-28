b07ec4f047bcae7caae3721841d501fa26b61078
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-11-28 23:08:16 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,32 @@
 class SongsController < ApplicationController
 
-end 
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
+  end
+
+end
